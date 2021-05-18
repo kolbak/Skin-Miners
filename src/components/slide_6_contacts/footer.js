d0d029baa8391ead4b65a08940f2ef43b63d3f40
Florn67 <edgewalker909@gmail.com>
2021-05-18 11:57:29 +0300

    images

--- src/components/slide_6_contacts/footer.js
+++ src/components/slide_6_contacts/footer.js
@@ -7,8 +7,11 @@ import '../../styles/components/slide_6_contacts/footer.scss'
 
 const Footer = () => {
   return (
-    <div className="wrap-slide">
-
+    <div className="wrap-slide slide-3">
+      <h1 className="h1-main">
+        <span className="slide-number"> 3 </span>
+        Почему мы ?
+      </h1>
     </div>
   )
 }
