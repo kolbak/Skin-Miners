a228bacfafc6855ac8df2faa28f32011f38316e2
mskKote <msk.vitaly@gmail.com>
2021-05-23 21:12:29 +0300

    slide_1_welcome hidden pictures

--- src/components/slide_0_welcome/welcomeSlide.js
+++ src/components/slide_0_welcome/welcomeSlide.js
@@ -1,12 +1,19 @@
 import * as React from "react";
-
 import Chart from "./chart";
 
 import "../../styles/components/slide_0_welcome/welcomeSlide.scss";
 
+import hidden_triangle from '../../images/slide_0_welcome/triangle.svg'
+import hidden_square from '../../images/slide_0_welcome/square.svg'
+
+
 const WelcomeSlide = () => {
   return (
-    <div className="First-slide">
+    <div className="welcome-slide">
+
+      <img id="hidden_triangle" alt="triangle" src={hidden_triangle} />
+      <img id="hidden_square" alt="square" src={hidden_square} />
+
       <div className="welcome">
         <h1 className="h1-skin-miners">
           Skin <br /> Miners
