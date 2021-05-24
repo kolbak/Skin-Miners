0e089f0e0aa55fbe18dac5763a6f0b032bb6e269
mskKote <msk.vitaly@gmail.com>
2021-05-24 19:11:29 +0300

    slide 2

--- src/components/slide_2_how/login.js
+++ src/components/slide_2_how/login.js
@@ -13,16 +13,21 @@ const Login = () => {
       <i className="bg-line n-4" />
       <i className="bg-line n-5" />
 
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      <form className="col-3">
+        <div className="mb-3"><h4 style={{ textAlign: "center" }}>Форма входа</h4></div>
+        <br />
+        <div className="mb-3"><input placeholder="Email" type="email" className="form-control" id="inputEmail" /></div>
+        <div className="mb-3"><input placeholder="Пароль" type="password" className="form-control" id="inputPassword" /></div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="check" />
+          <label class="form-check-label" for="check">Check me out</label>
         </div>
-        <div class="mb-3"><input type="password" class="form-control" id="exampleInputPassword1" /></div>
-        <div class="mb-3 form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1" /></div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+        <div className="mb-3"><a href="Регистрация" className="mt-3">Регистрация</a></div>
+
       </form>
+      <div className="blue-back"></div>
     </div>
   )
 }
