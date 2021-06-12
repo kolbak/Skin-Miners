b9da269851a938724e732e74d728b8ef96f4e7a3
mskKote <msk.vitaly@gmail.com>
2021-06-13 02:49:43 +0300

    chart

---
+++ src/components/slide_0_welcome/chart-line.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import '../../styles/components/slide_0_welcome/chart.scss'
+import "../../styles/components/slide_0_welcome/slide_0_mobile.scss";
+
+const ChartLine = ({ current, max }) => {
+  console.log(current, max);
+  return <div style={{ width: `calc(${current / max * 100}% - 0.35rem)` }} className='line-bg-inner' />
+}
+
+export default ChartLine
--- src/components/slide_0_welcome/chart.js
+++ src/components/slide_0_welcome/chart.js
@@ -1,35 +1,68 @@
 import * as React from "react"
+import ChartLine from "./chart-line";
 
 import '../../styles/components/slide_0_welcome/chart.scss'
-import "../../styles/components/slide_0_welcome/slide_0_mobile.scss";
+import '../../styles/components/slide_0_welcome/slide_0_mobile.scss'
 
 import user_default from '../../images/slide_0_welcome/user_default.jpg'
 
-const data = [
-  { name: 'Патрик', number: 7805.99, src: user_default },
-  { name: 'Стив', number: 7403.87, src: user_default },
-  { name: 'Анна', number: 6844.05, src: user_default },
-  { name: 'Александр', number: 5985.84, src: user_default },
-  { name: 'Полина', number: 5884.32, src: user_default }
-]
+const initialData = {
+  users: [
+    { name: '1', total: 1 },
+    { name: '2', total: 2 },
+    { name: '3', total: 3 },
+    { name: '4', total: 4 },
+    { name: '5', total: 5 }],
+  max: 5
+}
 
-const Chart = () => (<div className="wrap-chart">
-  <div id="chart">
-    {data.map((item, i) => (
-      <div className={`item  ${i ? '' : 'first-num'}`} key={`${item.name}-${i}`}>
-        <span className={`serial-number ${i ? '' : `first-num`}`}><span>{i + 1}</span></span>
-        <img className="avatar" src={item.src} alt="avatar" />
-        <div className="name-line">
-          <span className='name' children={item.name} />
-          <div className='line-bg' children={<div className='line-bg-inner' />} />
-          <span className='number-mobile' children={item.number} />
+const Chart = () => {
+
+  const [miners, setMiners] = React.useState(initialData)
+  const [loading, setLoading] = React.useState(true)
+
+  async function update() {
+    if (!loading) setLoading(true)
+
+    fetch("https://miningskins.com/api/top-users", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(resp => resp.json())
+      // .then(resp => { console.log(resp); return resp })
+      .then(resp => setMiners({
+        users: resp.users.sort((a, b) => b.total - a.total),
+        max: Math.max(...resp.users.map(x => x.total))
+      }))
+      .finally(_ => setTimeout(() => setLoading(false), 300))
+  }
+
+  React.useEffect(update, [])
+
+  return <div className="wrap-chart">
+    <div id="chart">
+      {miners.users.map((item, i) => (
+        <div className={`item  ${i ? '' : 'first-num'}`} key={`${item.name}-${i}`}>
+          <span className={`serial-number ${i ? '' : `first-num`}`}><span>{i + 1}</span></span>
+          <img className="avatar  " src={item.src ?? user_default} alt="avatar" />
+          <div className="name-line">
+            <span className='name' children={item.name} />
+            <div className='line-bg' children={<ChartLine current={item.total} max={miners.max} />} />
+            <span className='number-mobile' children={item.total} />
+          </div>
+          <span className='number' children={item.total} />
         </div>
-        <span className='number' children={item.number} />
-      </div>
-    ))}
+      ))}
+    </div>
+    <button className="btn" onClick={update} disabled={loading}>
+      {loading && <>
+        <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true" />
+        <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true" />
+        <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true" />
+      </>}
+      {!loading && "Обновить"}
+    </button>
   </div>
-  <button className="btn" children={"Обновить"} />
-
-</div>)
+}
 
 export default Chart
