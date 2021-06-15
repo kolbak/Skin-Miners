88be59b447cb6f59e6d6f187389e570308514bbe
mskKote <msk.vitaly@gmail.com>
2021-06-15 09:15:42 +0300

    registration logic

--- src/components/slide_4_registration/fourthSlide.js
+++ src/components/slide_4_registration/fourthSlide.js
@@ -28,26 +28,45 @@ const FourthSlide = () => {
   const [width, setWidth] = useState()
   const [loading, setLoading] = useState(false);
 
-  const [notEmail, setNotEmail] = useState(false);
-  const [notNickname, setNotNickname] = useState(false);
-  const [notSame, setNotSame] = useState(false);
-  const [notSteamLink, setNotSteamLink] = useState(false);
+  const [notEmail, setNotEmail] = useState();
+  const [notNickname, setNotNickname] = useState();
+  const [notPassword, setNotPassword] = useState();
+  const [notPasswordRepeat, setNotPasswordRepeat] = useState();
+  const [notSteamLink, setNotSteamLink] = useState();
 
   const password = useRef(null);
   const passwordRepeat = useRef(null);
   //* ||||||||||||||||||||||||||||||||
 
+  function checkEmail(e) {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexArr = e.target.value.match(regex);
+    console.log('checkEmail :>> ', regex.test(e.target.value));
+    if (e.target.value.length === 0)
+      setNotEmail(undefined)
+    else
+      setNotEmail(!regex.test(e.target.value))
+  }
+  function checkNickname(e) {
+    if (e.target.value.length === 0)
+      setNotNickname(undefined)
+    else
+      setNotNickname(false)
+  }
+  function checkPassword(e) {
+    passwordRepeat.current.value = "";
+    setNotPasswordRepeat(undefined)
+    setNotPassword(e.target.value.length === 0 ? undefined : false)
+  }
   function checkSame(e) {
-
     const pass = password.current.value
     const passRepeat = e.target.value
+    console.log(checkSame);
+    if (passRepeat.length === 0 || pass.length === 0)
+      return setNotPasswordRepeat(undefined);
+    if (pass === passRepeat)
+      return setNotPasswordRepeat(false)
 
-    if (passRepeat.length === 0
-      || pass === passRepeat
-      || pass.length === 0) {
-      setNotSame(false);
-      return;
-    }
     let error = pass.length < passRepeat.length;
 
     for (let i = 0; i < passRepeat.length; i++) {
@@ -55,22 +74,18 @@ const FourthSlide = () => {
         error = true; break;
       }
     }
-    // console.group('same');
-    // console.log(formData.password);
-    // console.log(e.target.value);
-    // console.log(error);
-    // console.groupEnd();
-    setNotSame(error);
+    setNotPasswordRepeat(error);
   }
   function checkSteamLink(e) {
     const regexArr = e.target.value.match(
       /^https:\/\/steamcommunity\.com\/tradeoffer\/new\/\?partner=\w*&token=\w*(?!&)$/);
 
-    setNotSteamLink(
-      e.target.value.length !== 0 &&
-      (regexArr === null || regexArr.length !== 1)
-    )
+    if (e.target.value.length === 0)
+      setNotSteamLink(undefined)
+    else
+      setNotSteamLink(regexArr === null || regexArr.length !== 1)
   }
+
 
   function hideError() {
     setErrorMessage({ isError: false, message: "" });
@@ -148,13 +163,11 @@ const FourthSlide = () => {
     <img id="hidden_triangle" alt="hidden_triangle" src={hidden_triangle} />
 
     <form id="formRegistration" onSubmit={signUp}>
-      {errorMessage.isError && <p className="error">
-        {errorMessage.message}
-      </p>}
+      {errorMessage.isError && <p className="error" children={errorMessage.message} />}
 
       <div className="mb-custom-4">
-        <input type="email" name="login" required placeholder="Email" disabled={loading}
-          className={"form-control g-light " + (notNickname ? "errorInput errorInputReg" : "")} />
+        <input type="email" name="login" required placeholder="Email" disabled={loading} onBlur={checkEmail}
+          className={"form-control " + (notEmail === undefined || notEmail === null ? "" : notEmail === true ? "errorInput errorInputReg r-light" : "g-light")} />
         {width > 1000 && notEmail && <>
           <p id="hint_email_text" className="hint">Амм, тут должен быть email... <br />
             <span className="orange">( имейл )</span>
@@ -163,8 +176,8 @@ const FourthSlide = () => {
         </>}
       </div>
       <div className="mb-custom-4">
-        <input name="name" id="inputNickname" placeholder="Никнейм" required disabled={loading}
-          className={"form-control g-light " + (notNickname ? "errorInput errorInputReg" : "")} />
+        <input name="name" id="inputNickname" placeholder="Никнейм" required disabled={loading} onChange={checkNickname}
+          className={"form-control " + (notNickname === undefined ? "" : notNickname === true ? "errorInput errorInputReg r-light" : "g-light")} />
         {width > 1000 && notNickname && <>
           <p id="hint_nickname_text" className="hint">Данный никнейм уже занят кем-то другим <br />
             <span className="orange">( попробуйте ещё какой-нибудь )</span>
@@ -173,15 +186,15 @@ const FourthSlide = () => {
         </>}
       </div>
       <div className="mb-custom-4">
-        <input type="password" className={"form-control g-light " + (notSame ? "errorInput errorInputReg" : "")} ref={password}
-          name="password" id="inputPassword" placeholder="Пароль" disabled={loading}
-          onChange={_ => { passwordRepeat.current.value = ""; setNotSame(false); }} required />
+        <input type="password" placeholder="Пароль" disabled={loading} required
+          className={"form-control " + (notPassword === undefined ? "" : notPassword === true ? "errorInput errorInputReg r-light" : "g-light")}
+          ref={password} onChange={checkPassword} name="password" id="inputPassword" />
       </div>
       <div className="mb-custom-4">
-        <input type="password" className={"form-control r-light " + (notSame ? "errorInput errorInputReg" : "")} ref={passwordRepeat}
-          onChange={checkSame} name="passwordRepeat" id="inputPasswordCheck" disabled={loading}
-          placeholder="Проверка пароля" required />
-        {width > 1000 && notSame && <>
+        <input type="password" placeholder="Проверка пароля" disabled={loading} required
+          className={"form-control " + (notPasswordRepeat === undefined ? "" : notPasswordRepeat === true ? "errorInput errorInputReg r-light " : "g-light")}
+          ref={passwordRepeat} onChange={checkSame} name="passwordRepeat" id="inputPasswordCheck" />
+        {width > 1000 && notPasswordRepeat && <>
           <p id="hint_password_check_text" className="hint">Пароли должны быть одинаковыми <br />
             <span className="orange">( потому и проверка )</span>
           </p>
@@ -190,86 +203,19 @@ const FourthSlide = () => {
       </div>
       <div className="mb-custom-4">
         <input type="link" name="steam_url" id="inputSteamLink" disabled={loading}
-          className={"form-control " + (notSteamLink ? "errorInput errorInputReg" : "")}
+          className={"form-control " + (notSteamLink === undefined ? "" : notSteamLink === true ? "errorInput errorInputReg r-light" : "g-light")}
           onChange={checkSteamLink} placeholder="Ссылка на стим" required />
         {width > 1000 && notSteamLink && <>
           <p id="hint_steam_text" className="hint">Для нахождения этой ссылки, <a href="http://steamcommunity.com/my/tradeoffers/privacy">перейдите на официальный сайт Steam</a></p>
           <img id="hint_steam" className="hint-arrow" alt="hint_steam" src={hint_steam} />
         </>}
       </div>
-      <button style={{ marginTop: "1rem" }} disabled={loading} type="submit" className="btn btn-primary btn-lg d-block w-75 m-auto" children={"Регистрация"} />
+      <button style={{ marginTop: "1rem" }} type="submit" className="btn btn-primary btn-lg d-block w-75 m-auto"
+        disabled={loading || (notEmail ?? true) || (notNickname ?? true) || (notPassword ?? true) || (notPasswordRepeat ?? true) || (notSteamLink ?? true)}>
+        Регистрация
+      </button>
     </form>
   </div>)
-
-  // function inputData(e) {
-  //   setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }));
-  // }
-  // function inputPassword(e) {
-  //   setFormData(prev => ({ ...prev, [e.target.name]: e.target.value, passwordRepeat: "" }));
-  //   setNotSame(false);
-  // }
-
-
-  // return (
-  //   <div className="wrap-slide">
-  //     <h1 className="h1-main">
-  //       <span className="slide-number">4</span>
-  //       <span className="slide-title">Регистрация</span>
-  //     </h1>
-  //     <form action="" onSubmit={signUp}>
-  //       <h3>Регистрация</h3>
-
-  //       {errorMessage.isError ?
-  //         <p className="error" style={{ marginTop: "-1.5rem" }}>{errorMessage.message}</p> :
-  //         <br />}
-
-  //       <div className="form-group">
-  //         <label className="next-in-group">→ Логин</label>
-  //         <input value={formData.login} onChange={(e) => inputData(e)} name="login"
-  //           type="login" className="form-control" placeholder="Ваш неповторимый логин" required />
-
-  //         <label className="next-in-group">→ Никнейм</label>
-  //         <input value={formData.name} onChange={(e) => inputData(e)} name="name"
-  //           className="form-control" placeholder="Ваш интересный никнейм" required />
-  //       </div>
-
-  //       <div className="form-group">
-  //         <label className="next-in-group">→ Пароль</label>
-  //         <input value={formData.password} onChange={(e) => inputPassword(e)} name="password"
-  //           type="password" className={"form-control " + (notSame ? "errorInput" : "")}
-  //           placeholder="Ваш неприступный пароль" required />
-
-  //         <label className="next-in-group">→ Проверка пароля</label>
-  //         <input value={formData.passwordRepeat}
-  //           onChange={(e) => { inputData(e); checkSame(e) }} name="passwordRepeat"
-  //           type="password" className={"form-control " + (notSame ? "errorInput" : "")}
-  //           placeholder="Не могли бы вы его повторить?" required />
-  //         {notSame && <small className="errorSmall">Пароли должны совпадать</small>}
-  //       </div>
-  //       <br />
-
-  //       <div className="form-group">
-  //         <label>→ Ваша ссылка Steam</label>
-  //         <input value={formData.steam_url}
-  //           onChange={(e) => { inputData(e); checkSteamLink(e) }} name="steam_url"
-  //           type="text" className={"form-control " + (notSteamLink ? "errorInput" : "")}
-  //           placeholder="https://steamcommunity.com/tradeoffer/new/?partner=[данные]&token=[токен]" required />
-  //         {notSteamLink && <><small className="errorSmall">Некорректная ссылка</small><br /></>}
-  //         <small>Для нахождения этой ссылки, <a href="http://steamcommunity.com/my/tradeoffers/privacy">перейдите на официальный сайт Steam</a></small>
-  //       </div>
-  //       <br />
-
-  //       <button type="submit" className="btn btn-primary btn-lg btn-block">
-  //         {loading && <span className="spinner-grow spinner-grow-sm loader-left" style={{ right: "1rem" }} role="status" aria-hidden="true"></span>}
-  //         {loading ? <>Создание аккаунта</> : <>Зарегистрироваться</>}
-  //         {loading && <span className="spinner-grow spinner-grow-sm loader-right" style={{ left: "1rem" }} role="status" aria-hidden="true"></span>}
-  //       </button>
-  //       <br />
-
-  //     </form>
-
-  //   </div>
-  // )
 }
 
 export default FourthSlide
